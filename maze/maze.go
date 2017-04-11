cbdd5a3c046d62cbf96497a4898d29652eaf2492
vanya <vanya.s.vanya@gmail.com>
2017-04-11 18:39:20 +0300

    Add maze generator

---
+++ maze/maze.go
@@ -0,0 +1,29 @@
+package maze
+
+import "math/rand"
+
+var celltypes = map[int]string{0:"o", 1:"v", 2:"v", 3:"v"}
+
+func GenerateMaze(size int) [][]string  {
+	rows := make([][]string, size)
+	for i:=0; i<size; i++{
+		cols := make([]string, size)
+		for j:=0; j<size; j++{
+			cols[j]= getCell()
+		}
+		rows[i] = cols
+	}
+	setEndStart(&rows, size)
+	return rows
+}
+
+func setEndStart(maze *[][]string, size int){
+	start := rand.Intn(size*size)
+	end := rand.Intn(size*size)
+	(*maze)[start/100][start % 100] = "s"
+	(*maze)[end/100][end % 100] = "e"
+}
+
+func getCell() string {
+	return celltypes[rand.Intn(3)]
+}
